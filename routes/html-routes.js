7c4ee6031a1199c3082ac91cf0c7d9751d60db6c
Shelby Reyes <42514855+sdreyes@users.noreply.github.com>
2019-03-11 21:18:25 -0400

    Pass in user ID as a response so the user can be referenced when creating relationships between the movies and the user

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,6 +2,24 @@ var db = require("../models");
 
 module.exports = function(app) {
     app.get("/", function(req, res) {
-        res.render("index");
+        if (req.user) {
+            db.user.findOne({
+                where: {
+                    id: req.user.id
+                }
+            }).then(function (dbUser) {
+                var hbsObject = {
+                    user: req.user,
+                    username: dbUser.username
+                };
+                res.render("index", hbsObject);
+            });
+        }
+        else {
+            var hbsObject = {
+                user: req.user
+            };
+            res.render("index", hbsObject);
+        }
     });
 };
